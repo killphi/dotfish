72a4f223ce1b2d52e3474577401a24369fd04926
killphi <killphi+git@gmail.com>
2014-10-23 08:01:15 +0200

    changed specifity of node modules

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,16 +8,16 @@ source ~/.config/fish/aliases.fish
 set -g __killphi_tool_path ~/.config/fish/tools
 
 # modify path
+if not contains $HOME/node_modules/.bin $PATH
+  set -x PATH ~/node_modules/.bin $PATH
+end
+
 if not contains $HOME/bin $PATH
   set -x PATH ~/bin $PATH
 end
 
 if not contains $HOME/.local/bin $PATH
   set -x PATH ~/.local/bin $PATH
-end
-
-if not contains $HOME/node_modules/.bin $PATH
-  set -x PATH ~/node_modules/.bin $PATH
 end
 
 # VIm <3
@@ -27,5 +27,5 @@ set -x EDITOR $VISUAL
 # dircolors
 set -x LS_COLORS (dircolors .dir_colors | cut -d\n -f1 | sed -e 's/.*\'\(.*\)\'.*/\1/')
 
-# call rvm once, to set up env
+# call rvm once (silently), to set up env
 rvm ^&1 >/dev/null
