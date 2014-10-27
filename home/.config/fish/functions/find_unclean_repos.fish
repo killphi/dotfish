f3ac4013cd54365dd4d087de7a1c0b1b3eba5e9b
killphi <killphi+git@gmail.com>
2014-10-27 13:33:39 +0100

    fixup

--- home/.config/fish/functions/find_unclean_repos.fish
+++ home/.config/fish/functions/find_unclean_repos.fish
@@ -1,7 +1,7 @@
 function find_unclean_repos
   set -l unclean $__killphi_tool_path/git/unclean_repo.fish
-  set -l excludes node_modules bower_components vendor
-  set -l expression "-type d -name .git -prune -execdir $unclean"
+  set -l excludes "node_modules bower_components vendor"
+  set -l expression "-type d -name .git -prune -execdir $unclean \;"
 
-  find_excluding . $excludes $expression \\\; ^/dev/null
+  find_excluding . $excludes $expression ^/dev/null
 end
