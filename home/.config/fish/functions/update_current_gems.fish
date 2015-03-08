2e4fc967603ab30c12a6c1d3bcd8d1e7a88fc910
killphi <killphi+git@gmail.com>
2015-03-08 11:44:28 +0100

    include cleanup in updates

--- home/.config/fish/functions/update_current_gems.fish
+++ home/.config/fish/functions/update_current_gems.fish
@@ -4,10 +4,14 @@ function update_current_gems
 
   rvm @global do gem update --system
   and rvm @global do gem update
+  and rvm @global do gem cleanup
 
   if test -f Gemfile
     bundle update
+    bundle clean --force
+    gem cleanup
   else
     gem update
+    gem cleanup
   end
 end
