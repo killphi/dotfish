f29d386f49ef229a8facfc734753f3494ca174c5
killphi <killphi+git@gmail.com>
2015-10-09 01:06:50 +0200

    improve UXRVT startup

--- home/.config/fish/functions/term.fish
+++ home/.config/fish/functions/term.fish
@@ -1,9 +1,16 @@
 #!/usr/bin/env fish
 function term
+  # wait if started in parallel
+  sleep 0.1
+
   # if URXVT is already running, do nothing
-  if ps -C urxvt256c-ml > /dev/null
+  if ps -C urxvt256c-ml
     return
   end
+
+  # re-read user .Xresources
+  test -r ~/.Xresources; and xrdb -merge -I$HOME ~/.Xresources
+  sleep 0.1
 
   # start 256 color URXVT and attach tmux session
   urxvt256c-ml -cd ~ -C -e tmux -2 attach -t standard &
@@ -14,5 +21,5 @@ function term
   end
 
   # make window fullscreen
-  wmctrl -r tmux -b add,fullscreen
+  wmctrl -r tmux -t 1 -e 0,0,0,-1,-1 -b add,fullscreen
 end
