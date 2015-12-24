6af143b68e8f6c3949b94906c92d5c4464caf6f3
killphi <killphi+git@gmail.com>
2015-12-24 16:35:30 +0100

    cosmetic changes to ruby dir shortener

--- home/.config/fish/ext/dir_shortener.rb
+++ home/.config/fish/ext/dir_shortener.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
-# shortens /very/long/directory/path to …/directory/path
+# shortens /very/long/directory/path to .../directory/path
 # also: /home/dir/path to ~/path
 
 inputs = $stdin.read.sub(/^#{ENV['HOME']}/, '~').split('/').last(3)
 
-if inputs.size == 3
-  inputs[0] = '…' unless inputs.first == '~'
-end
+inputs[0] = '…' if inputs.size == 3 && inputs.first != '~'
 
 puts inputs.join('/')
